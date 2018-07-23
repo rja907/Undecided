f917bd0c0c44074297ee8a8351935b06f2bb32d1
Raj <kmrrajj@gmail.com>
2018-07-22 17:34:02 -0700

    added ItemList component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppNavBar from './components/AppNavBar';
+import ItemList from './components/ItemList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppNavBar />
-        <h1>Welcome to React</h1>
+        <ItemList />
       </div>
     );
   }
