3a42f507f2654e4dffd1a1c0d2ef02a68cae5473
Raj <kmrrajj@gmail.com>
2018-07-27 23:38:15 -0700

    added Provider store to the App

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 import AppNavBar from './components/AppNavBar';
 import ItemList from './components/ItemList';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavBar />
-        <ItemList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar />
+          <ItemList />
+        </div>
+      </Provider>
     );
   }
 }
