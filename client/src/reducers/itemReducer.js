7dcd70e4aeb82f1f2686ca171bca3c5ec210758c
Raj <kmrrajj@gmail.com>
2018-07-29 21:21:07 -0700

    added functionality to delete item

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -14,6 +14,11 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
       return { ...state };
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== action.payload)
+      };
     default:
       return state;
   }
