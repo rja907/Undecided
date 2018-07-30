b42d83e463426a09bfd9c70ee200ccddeb821842
Raj <kmrrajj@gmail.com>
2018-07-29 21:23:50 -0700

    added modal to add new items

---
+++ client/src/components/ItemModal.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from 'react-strap';
+import { connect } from 'react-redux';
+import { addItem } from '../actions/ItemActions';
+
+export default class ItemModal extends Component {
+  render() {
+    return <div />;
+  }
+}
